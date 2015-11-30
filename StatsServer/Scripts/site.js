9e856204603b9b2e14f8e0a3b3894c279f117302
Abdullah Saleem <a.saleem2993@gmail.com>
2015-12-01 01:37:14 +0500

    Server Basics

---
+++ StatsServer/Scripts/site.js
@@ -0,0 +1,9 @@
+﻿var hub = $.connection.statsHub;
+
+$.extend(hub.client, {
+    sendStats: function (cpuLoad, cpuFreq, cpuTemp, ram) {
+        console.log(cpuLoad, cpuFreq, cpuTemp, ram);
+    }
+});
+
+$.connection.hub.start();
---
+++ StatsServer/StatsReportHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace StatsServer
+{
+    [HubName("statsReportHub")]
+    public class StatsReportHub:Hub
+    {
+        [HubMethodName("reportStats")]
+        public void ReportStats(int cpuLoad, int cpuFreq, int cpuTemp, int ram)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<StatsHub>()
+                .Clients.All()
+                .sendStats(cpuLoad, cpuFreq, cpuTemp, ram);
+        }
+    }
+}
