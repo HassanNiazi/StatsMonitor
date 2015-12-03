e3e019ad3fd811f31ec181512998b6e78dfdba38
Abdullah Saleem <a.saleem2993@gmail.com>
2015-12-03 15:28:14 +0500

    MVVM T1

--- DesktopClient/MainWindow.xaml.cs
+++ DesktopClient/MainWindow.xaml.cs
@@ -1,20 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
-using Microsoft.AspNet.SignalR.Client;
+﻿using System.Windows;
 
 namespace DesktopClient
 {
@@ -23,60 +7,11 @@ namespace DesktopClient
     /// </summary>
     public partial class MainWindow
     {
-        private readonly StatsView _statsView;
-
-        private readonly PerformanceCounter _perfCountCpuLoad = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
-        private readonly PerformanceCounter _perfCountCpuTemp = new PerformanceCounter("Thermal Zone Information", "Temperature", @"\_TZ.THM");
-        private readonly PerformanceCounter _perfCountSysMem = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-        private readonly PerformanceCounter _perfCountFreq = new PerformanceCounter("Processor Information", "Processor Frequency", "_Total");
-
-        private readonly DispatcherTimer _dispatcherTimer = new DispatcherTimer();
-
-        private const string ServerPath = "http://localhost:63637/signalr";
-
-        private readonly HubConnection _hubConnection;
-        private readonly IHubProxy _hubProxy;
-
-        private bool _connected;
-
+       
         public MainWindow()
         {
             InitializeComponent();
-            _statsView = (StatsView)DataContext;
             Left = SystemParameters.PrimaryScreenWidth - Width;
-
-            _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000);
-            _dispatcherTimer.Tick += _dispatcherTimer_Tick;
-            _dispatcherTimer.Start();
-
-            _hubConnection=new HubConnection(ServerPath);
-            _hubProxy =_hubConnection.CreateHubProxy("statsReportHub");
-            Connect();
-        }
-
-
-        public async void Connect()
-        {
-            try
-            {
-                await _hubConnection.Start();
-                _connected = true;
-            }
-            catch
-            {
-                //ignore
-            }
-        }
-
-        private void _dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            var stats = new Stats((int)_perfCountCpuLoad.NextValue(), (int)_perfCountFreq.NextValue(),(int)_perfCountCpuTemp.NextValue()-273, (int)_perfCountSysMem.NextValue());     
-            _statsView.CurrentStats = stats;
-            _dispatcherTimer.Start();
-            if (_connected)
-            {
-                _hubProxy.Invoke("reportStats", stats.CpuLoad, stats.CpuFreq, stats.CpuTemp, stats.Ram);
-            }
         }
     }
 }
--- DesktopClient/StatsView.cs
+++ DesktopClient/StatsView.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Threading;
 using DesktopClient.Annotations;
+using Microsoft.AspNet.SignalR.Client;
 
 namespace DesktopClient
 {
     public class StatsView : INotifyPropertyChanged
     {
+        private readonly PerformanceCounter _perfCountCpuLoad = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        private readonly PerformanceCounter _perfCountCpuTemp = new PerformanceCounter("Thermal Zone Information", "Temperature", @"\_TZ.THM");
+        private readonly PerformanceCounter _perfCountSysMem = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        private readonly PerformanceCounter _perfCountFreq = new PerformanceCounter("Processor Information", "Processor Frequency", "_Total");
+
+        private readonly DispatcherTimer _dispatcherTimer = new DispatcherTimer();
+
+        private const string ServerPath = "http://localhost:63637/signalr";
+
+        private readonly HubConnection _hubConnection;
+        private readonly IHubProxy _hubProxy;
+
+        private bool _connected;
+
+
         private Stats _stats = new Stats();
 
         public Stats CurrentStats
@@ -20,6 +34,41 @@ namespace DesktopClient
             {
                 _stats = value;
                OnPropertyChanged();
+            }
+        }
+
+        public StatsView()
+        {
+            _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            _dispatcherTimer.Tick += _dispatcherTimer_Tick;
+            _dispatcherTimer.Start();
+
+            _hubConnection = new HubConnection(ServerPath);
+            _hubProxy = _hubConnection.CreateHubProxy("statsReportHub");
+            Connect();
+        }
+
+        public async void Connect()
+        {
+            try
+            {
+                await _hubConnection.Start();
+                _connected = true;
+            }
+            catch
+            {
+                //ignore
+            }
+        }
+
+        private void _dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            var stats = new Stats((int)_perfCountCpuLoad.NextValue(), (int)_perfCountFreq.NextValue(), (int)_perfCountCpuTemp.NextValue() - 273, (int)_perfCountSysMem.NextValue());
+            CurrentStats = stats;
+            _dispatcherTimer.Start();
+            if (_connected)
+            {
+                _hubProxy.Invoke("reportStats", stats.CpuLoad, stats.CpuFreq, stats.CpuTemp, stats.Ram);
             }
         }
 
