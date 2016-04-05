f4978f85e4a11929141d5e5594940d3b0bf5e908
abdullah2993 <a.saleem2993@gmail.com>
2016-04-05 19:36:41 +0500

    No more jQuery...Knockout yeehaa

--- StatsServer/Scripts/site.js
+++ StatsServer/Scripts/site.js
@@ -1,19 +1,46 @@
 ﻿var hub = $.connection.statsHub;
+var stats = ko.observableArray();
+//$.extend(hub.client, {
+//    updateStats: function (id, specs, cpu, ram) {
+//        if ($("#" + id).length) {
+//            $("#" + id).html("<td>" + specs + "</td><td>" + cpu + "</td><td>" + ram + "</td>");
+//        } else {
+//            $("#tbl").append("<tr id='" + id + "'><td>" + specs + "</td><td>" + cpu + "</td><td>" + ram + "</td>");
+//        }
+            
+//    },
+//    removeStats: function(id) {
+//        if ($("#" + id).length) {
+//            $("#" + id).remove();
+//        }
+//    }
+//});
+
+var Stat = function (id, specs, cpu, ram) {
+    var self = this;
+    self.id = id;
+    self.specs = ko.observable(specs);
+    self.cpu = ko.observable(cpu);
+    self.ram = ko.observable(ram);
+};
 
 $.extend(hub.client, {
-    updateStats: function (id, specs, cpu, ram) {
-        if ($("#" + id).length) {
-            $("#" + id).html("<td>" + specs + "</td><td>" + cpu + "</td><td>" + ram + "</td>");
+    updateStats: function(id, specs, cpu, ram) {
+        
+        var i = ko.utils.arrayFirst(stats(), function (item) { return item.id === id });
+        if (i == undefined) {
+            stats.push(new Stat(id, specs, cpu, ram));
         } else {
-            $("#tbl").append("<tr id='" + id + "'><td>" + specs + "</td><td>" + cpu + "</td><td>" + ram + "</td>");
+            i.specs(specs);
+            i.cpu(cpu);
+            i.ram(ram);
         }
-            
+
     },
     removeStats: function(id) {
-        if ($("#" + id).length) {
-            $("#" + id).remove();
-        }
+        stats.remove(function(i) { return i.id === id; });
     }
 });
 
+ko.applyBindings(stats);
 $.connection.hub.start();
